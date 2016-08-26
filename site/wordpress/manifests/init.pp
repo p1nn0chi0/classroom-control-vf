5c1ec2b83961d720c399fe88ebdc6d4e1c6dd8c9
p1nn0chi0 <scott.gousman@norseman.com>
2016-08-26 17:14:01 -0500

    Update init.pp

--- site/wordpress/manifests/init.pp
+++ site/wordpress/manifests/init.pp
@@ -4,3 +4,20 @@ class { 'wordpress':
   db_user     => 'wordpress',
   db_password => 'hvyH(S%t(\"0\"16',
 }
+
+class { 'wordpress':
+  version => '3.4',
+}
+
+class { 'wordpress':
+  install_dir => '/var/www/wordpress',
+}
+
+class { 'wordpress':
+  wp_multisite   => true,
+  wp_site_domain => 'blog.domain.com',
+}
+
+class { 'wordpress':
+  wp_additional_config => 'foo/wp-config-extra.php.erb',
+}
