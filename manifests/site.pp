766f9c2b554bed82af1f9fac345d265bf0d194a1
p1nn0chi0 <scott.gousman@norseman.com>
2016-08-26 11:18:15 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  }
  
 if $::virtual != 'physical' {
 $vmname = capitalize($::virtual)
