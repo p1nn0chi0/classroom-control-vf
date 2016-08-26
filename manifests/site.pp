fbfb182c438e0d513b623a13170c48101cf43614
p1nn0chi0 <scott.gousman@norseman.com>
2016-08-26 14:53:47 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,10 +16,8 @@
 # they run. The Puppet Enterprise console needs this to display file contents
 # and differences.
 
-# Disable filebucket by default for all File resources:
 File { backup => false }
 
-# Randomize enforcement order to help understand relationships
 ini_setting { 'random ordering':
   ensure  => present,
   path    => "${settings::confdir}/puppet.conf",
@@ -28,20 +26,7 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
   $message = hiera('message')
   notify { $message: }
   include nginx
